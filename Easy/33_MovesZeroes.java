ed0af6822a7a250ef89ca5c0810d0ba29b07940a
dhirajgadekar <dhirajgadekar629@gmail.com>
2023-09-29 23:22:35 +0530

    Moves Zeroes

---
+++ Easy/33_MovesZeroes.java
@@ -0,0 +1,23 @@
+/*
+ * 33] Move Zeroes :
+ */
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int  j = 0;
+        int temp = 0;
+        for(int i = 0; i < nums.length; i++) {
+
+            if(nums[i] != 0 && nums[j] == 0) {
+
+                temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+            if(nums[j] != 0) {
+
+                j++;
+            }
+        }
+    }
+}
