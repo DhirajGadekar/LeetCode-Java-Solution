a693b09d30abba7af0e73b41b8d7a688a3240fce
dhirajgadekar <dhirajgadekar629@gmail.com>
2023-10-12 14:32:32 +0530

    Search in Rotated Sorted Array

---
+++ Medium/40_SearchinRotatedSortedArray.java
@@ -0,0 +1,17 @@
+/*
+ * 40] Search in Rotated Sorted Array
+ */
+
+class Solution {
+    public int search(int[] nums, int target) {
+
+        for(int i = 0; i < nums.length; i++) {
+
+            if(nums[i] == target) {
+
+                return i;
+            }
+        }
+        return -1;
+    }
+}
