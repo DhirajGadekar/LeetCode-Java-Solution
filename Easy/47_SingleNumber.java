9f1546b8fbc1e6e8d324363d1ece09acc21ad129
dhirajgadekar <dhirajgadekar629@gmail.com>
2023-10-27 21:54:14 +0530

    Single Number

---
+++ Easy/47_SingleNumber.java
@@ -0,0 +1,14 @@
+/*
+ * 47] Single Number :
+ */
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+        for(int i = 0; i < nums.length; i++) {
+
+            ans ^= nums[i];
+        }
+        return ans;
+    }
+}
