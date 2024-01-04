1de22a4dc5ebd1e6d51e5381d4d9313dd8f5e766
dhirajgadekar <dhirajgadekar629@gmail.com>
2024-01-04 12:39:49 +0530

    GreatestCommonDivisorofStrings

---
+++ Easy/79_GreatestCommonDivisorofStrings.java
@@ -0,0 +1,20 @@
+/*
+ * 79] Greatest Common Divisor of Strings
+ */
+
+class Solution {
+    private static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+    public String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+
+        int gcdLength = gcd(str1.length(), str2.length());
+        return str1.substring(0, gcdLength);
+    }
+}
